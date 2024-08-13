b2e556bf2d8ddfb017d0870e3306e980889575fb
antko35 <kovsheranton@gmail.com>
2024-08-13 13:23:52 +0300

    Initial commit

---
+++ backend/BookStore.Application/Services/BooksService.cs
@@ -0,0 +1,39 @@
+﻿using BookStore.Core.Models;
+using BookStore.DataAccess.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Application.Services
+{
+    public class BooksService : IBooksService
+    {
+        private readonly IBooksRepository _booksRepository;
+        public BooksService(IBooksRepository booksRepository)
+        {
+            _booksRepository = booksRepository;
+        }
+
+        public async Task<List<Book>> GetAllBooks()
+        {
+            return await _booksRepository.Get();
+        }
+
+        public async Task<Guid> CreateBook(Book book)
+        {
+            return await _booksRepository.Create(book);
+        }
+
+        public async Task<Guid> UpdateBook(Guid id, string title, string decsription, decimal price)
+        {
+            return await _booksRepository.Update(id, title, decsription, price);
+        }
+
+        public async Task<Guid> DeleteBook(Guid id)
+        {
+            return await _booksRepository.Delete(id);
+        }
+    }
+}
---
+++ backend/BookStore.DataAccess/Configurations/BookConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using BookStore.Core.Models;
+using BookStore.DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.DataAccess.Configurations
+{
+    public class BookConfiguration : IEntityTypeConfiguration<BookEntity>
+    {
+        public void Configure(EntityTypeBuilder<BookEntity> builder)
+        {
+            builder.HasKey(e => e.Id);
+
+            builder.Property(b => b.Title)
+                .HasMaxLength(Book.MAX_TITLE_LENGTH)
+                .IsRequired();
+
+            builder.Property(b => b.Description)
+                .IsRequired();
+
+            builder.Property(b => b.Price) 
+                .IsRequired();
+        }
+    }
+}
